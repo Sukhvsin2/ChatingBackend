9e162fdb179720ed506f4cdfb0882de95e68c6b7
Sukhvsin2 <sukhvsingh2026@gmail.com>
2020-11-20 15:20:55 +0530

    cors error

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
+const cors = require('cors')
 
-const PORT = process.env.PORT || 2727
+const PORT = process.env.PORT || 5000
+const router = require('./router')
 
-const app = express();
+const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+app.all('*', function(req, res, next) {
+       res.header("Access-Control-Allow-Origin", "*");
+       res.header("Access-Control-Allow-Headers", "X-Requested-With");
+       res.header('Access-Control-Allow-Headers', 'Content-Type');
+       next();
+});
+app.use(router)
+
+io.on('connection', (socket) => {
+    console.log('connected to socket!!');
+
+    socket.on('disconnect', () => {
+        console.log('User left!!');
+    })
+})
 
 server.listen(PORT, () => {
-    console.log('server at '+PORT);
+    console.log('server running at '+PORT);
 })
